ef2279dd1b44cc0c1240aa1555e50e384ed737a5
Ryan Rose <ryanprose96@gmail.com>
2020-02-06 21:16:53 -0500

    Added Basic Tile Generation
    There is currently No Object Pooling with this model, needs to be added for optimization

---
+++ Cruise Control/Assets/Scripts/CarTag.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarTag : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("collided with " + collision.gameObject.name);
+    }
+}
