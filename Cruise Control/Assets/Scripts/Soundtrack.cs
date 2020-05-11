a93e1883b7b0eb9e7882ca112c1d2e50ebdb7540
yamminezze <yezzedin@gmu.edu>
2020-05-11 11:34:23 -0400

    Ugh

---
+++ Cruise Control/Assets/Scripts/Soundtrack.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Soundtrack : MonoBehaviour
+{
+    public AudioClip[] soundtrack;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!GetComponent<AudioSource>().playOnAwake)
+        {
+            GetComponent<AudioSource>().clip = soundtrack[Random.Range(0, soundtrack.Length)];
+            //GetComponent<AudioSource>().volume = 0.01f;
+            GetComponent<AudioSource>().Play();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!GetComponent<AudioSource>().isPlaying)
+        {
+            GetComponent<AudioSource>().clip = soundtrack[Random.Range(0, soundtrack.Length)];
+            GetComponent<AudioSource>().Play();
+        }
+    }
+}
